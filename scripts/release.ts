20bdb2b0e04ddd9185a3ae57f8973ec3494f6043
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2023-09-15 18:58:40 +0300

    fix: test

--- scripts/release.ts
+++ scripts/release.ts
@@ -2,6 +2,10 @@ import { execSync } from 'node:child_process';
 
 const mode = process.argv[2] ?? 'patch';
 
+const gitStatus = execSync('git status -s').toString('utf-8');
+console.log('status: ', gitStatus, gitStatus.length);
+/*
+
 execSync(`npm version --workspaces --git-tag-version=false ${mode}`, {
   stdio: 'inherit'
 });
@@ -12,3 +16,4 @@ execSync(
 );
 
 execSync(`npm version ${mode}`, { stdio: 'inherit' });
+*/
