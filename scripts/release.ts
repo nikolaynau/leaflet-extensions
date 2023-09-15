4693d8baff26cf1af0f4605cf8f1b0bf8ee9ff19
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2023-09-15 18:48:54 +0300

    fix: scripts

--- scripts/release.ts
+++ scripts/release.ts
@@ -2,10 +2,13 @@ import { execSync } from 'node:child_process';
 
 const mode = process.argv[2] ?? 'patch';
 
-execSync(`npm version --workspaces --git-tag-version=false ${mode}`);
+execSync(`npm version --workspaces --git-tag-version=false ${mode}`, {
+  stdio: 'inherit'
+});
 
 execSync(
-  'git add packages/* && git commit -m "build: update workspace versions"'
+  'git add packages/* && git commit -m "build: update workspace versions"',
+  { stdio: 'inherit' }
 );
 
-execSync(`npm version ${mode}`);
+execSync(`npm version ${mode}`, { stdio: 'inherit' });
